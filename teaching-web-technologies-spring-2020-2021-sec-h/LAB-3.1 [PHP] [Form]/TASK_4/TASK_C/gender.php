3313196c7700bf77a444ba4b691a86eef2dbab99
M A Mohit Khan <m.a.mohit.khan@gmail.com>
2021-02-17 16:07:01 +0600

    gender input same page with fill

---
+++ teaching-web-technologies-spring-2020-2021-sec-h/LAB-3.1 [PHP] [Form]/TASK_4/TASK_C/gender.php
@@ -0,0 +1,28 @@
+<?php
+	
+	if(isset($_POST['submit']))
+	{
+		echo "Input Gender: ";
+		$yourGender = $_POST['gender'];
+		echo $yourGender;
+	}
+?>
+
+
+<html>
+<head>
+	<title>Gender form input diplay same page and fill</title>
+</head>
+<body>
+	<form method="POST" action="">
+		<fieldset>
+			<legend>GENDER</legend>
+          <input type="radio" name="gender" value="Male" <?php if(isset($_POST['gender']) && $_POST['gender'] === "Male") { echo "checked";}?> > Male
+          <input type="radio" name="gender" value="Female" <?php if(isset($_POST['gender']) && $_POST['gender'] === "Female") { echo "checked";}?> > Female
+          <input type="radio" name="gender" value="Others" <?php if(isset($_POST['gender']) && $_POST['gender'] === "Others" ) { echo "checked";}?> > Others
+			<hr>
+			<input type="submit" name = "submit" value="submit">
+		</fieldset>
+	</form>
+</body>
+</html>
