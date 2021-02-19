0fa7494da4d8da7b5b6b39809116e667128dc680
M A Mohit Khan <m.a.mohit.khan@gmail.com>
2021-02-20 00:04:47 +0600

    no3

---
+++ teaching-web-technologies-spring-2020-2021-sec-h/LAB-2 [PHP] [Placement]/no3.php
@@ -0,0 +1,14 @@
+<?php
+  $number=20;
+  if($number%2===0)
+    echo 'the number '."$number".' is even';
+  else
+    echo 'the number '."$number".' is odd';
+  
+  $number1=19;
+  echo "\n";
+  if($number1%2===0)
+    echo 'the number '."$number1".' is even';
+  else
+    echo 'the number '."$number1".' is odd';
+?>
