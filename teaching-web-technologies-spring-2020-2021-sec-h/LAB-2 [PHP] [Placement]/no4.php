d05a0600919f91814581d9f433a65dee9eca188a
M A Mohit Khan <m.a.mohit.khan@gmail.com>
2021-02-20 00:05:05 +0600

    no4

---
+++ teaching-web-technologies-spring-2020-2021-sec-h/LAB-2 [PHP] [Placement]/no4.php
@@ -0,0 +1,11 @@
+<?php
+  $number1=20;
+  $number2=30;
+  $number3=40;
+  if ($number1>$number2&&$number1>$number3)
+  echo "largest number is $number1";
+  elseif ($number2>$number1&&$number2>$number3)
+  echo "largest number is $number2";
+  if ($number3>$number2&&$number3>$number1)
+  echo "largest number is $number3";
+?>
